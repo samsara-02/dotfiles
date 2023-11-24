6547d059d06fb7049e4820f47466a64c3e5c4695
samsara-02 <fheg975@gmail.com>
2023-11-24 12:44:53 +0100

    The great mergure

---
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,5 @@
+https://github.com/ajeetdsouza/zoxide#configuration
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
+
+https://ohmyposh.dev/docs/installation/prompt
+oh-my-posh init pwsh --config "C:\Users\Sam\AppData\Local\Programs\oh-my-posh\themes\powerlevel10k_rainbow.omp.json" | Invoke-Expression
